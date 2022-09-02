e1d0d2eca477891e05a7f743a60b2af1e5e3eba0
gluknik <gluknik@gmail.com>
2022-09-02 21:18:24 +0300

    Fix

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,34 +1,50 @@
-import { AppProps } from "next/app"
-import { useState } from "react"
-import Head from "next/head"
+import { AppProps } from "next/app";
+import { useState } from "react";
+import Head from "next/head";
 // global styles
-import "../styles/globals.scss"
+import "../styles/globals.scss";
 // mantine theming
-import yourMantineTheme from "../themes/mantine"
-import { MantineProvider, ColorScheme, ColorSchemeProvider } from "@mantine/core"
-import { WalletContextWrapper } from "../context/wallet.context"
-import { NotificationsProvider } from "@mantine/notifications"
-import "../styles/styles.scss"
+import yourMantineTheme from "../themes/mantine";
+import {
+  MantineProvider,
+  ColorScheme,
+  ColorSchemeProvider,
+} from "@mantine/core";
+import { WalletContextWrapper } from "../context/wallet.context";
+import { NotificationsProvider } from "@mantine/notifications";
+import "../styles/styles.scss";
 
 export default function App(props: AppProps & { colorScheme: ColorScheme }) {
-  const { Component, pageProps } = props
-  const [colorScheme, setColorScheme] = useState<ColorScheme>(props.colorScheme)
+  const { Component, pageProps } = props;
+  const [colorScheme, setColorScheme] = useState<ColorScheme>(
+    props.colorScheme,
+  );
 
   const toggleColorScheme = (value?: ColorScheme) => {
-    const nextColorScheme = value || (colorScheme === "dark" ? "light" : "dark")
-    setColorScheme(nextColorScheme)
+    const nextColorScheme =
+      value || (colorScheme === "dark" ? "light" : "dark");
+    setColorScheme(nextColorScheme);
     // NOTE: if you want to, set cookie here
-  }
+  };
 
   return (
     <>
       <Head>
         <title>NFT Lootery</title>
-        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
-        <meta name="description" content="Don't miss the opportunity to easily win 1 ETH" />
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+        <meta
+          name="description"
+          content="Don't miss the opportunity to easily win 1 BNB"
+        />
       </Head>
 
-      <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
+      <ColorSchemeProvider
+        colorScheme={colorScheme}
+        toggleColorScheme={toggleColorScheme}
+      >
         <MantineProvider
           withGlobalStyles
           withNormalizeCSS
@@ -57,5 +73,5 @@ export default function App(props: AppProps & { colorScheme: ColorScheme }) {
         </MantineProvider>
       </ColorSchemeProvider>
     </>
-  )
+  );
 }
